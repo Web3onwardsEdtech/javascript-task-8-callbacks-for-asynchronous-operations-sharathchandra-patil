92938dcfe96ced6c3d38e1373e0cde9ec4d9510c
Sharathchandra <sharathchandra9204@gmail.com>
2024-10-05 17:39:50 +0530

    Task 8 submission

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function delayedExecution(callback, delay, ...args) {
+    setTimeout(() => {
+        callback(...args);
+    }, delay);
+}
+
+function greet(name, message) {
+    console.log(`Hello, ${name}! ${message}`);
+}
+
+delayedExecution(greet, 10000, 'John Doe', 'Welcome to the callback demonstration!');
+
+
 
